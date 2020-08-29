aff512c94e830ab45126a6b6f2cf408b9c6990ad
smishra11 <msubhasish11@gmail.com>
2020-08-29 23:19:57 +0530

    Working on removing product from basket

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,24 @@
 export const initialState = {
-  basket: [],
+  basket: [
+    {
+      id: "5",
+      title:
+        "Apple iPad Pro (11-inch, Wi-Fi, 128GB) - Space Grey (2nd Generation)",
+      price: 74999,
+      rating: 5,
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/81p1L85KinL._SX679_.jpg",
+    },
+    {
+      id: "5",
+      title:
+        "Apple iPad Pro (11-inch, Wi-Fi, 128GB) - Space Grey (2nd Generation)",
+      price: 74999,
+      rating: 5,
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/81p1L85KinL._SX679_.jpg",
+    },
+  ],
   user: null,
 };
 
@@ -12,7 +31,21 @@ const reducer = (state, action) => {
         basket: [...state.basket, action.item],
       };
     case "REMOVE_FROM_BASKET":
-      return { state };
+      let newBasket = [...state.basket];
+
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't remove product (id: ${action.id}) as its not in basket`
+        );
+      }
+
+      return { ...state, basket: newBasket };
     default:
       return state;
   }
